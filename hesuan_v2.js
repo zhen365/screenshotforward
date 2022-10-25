542bb9c5c1ee30d39a21e4845ad5c13daafe2033
zhen365 <5002779+zhen365@user.noreply.gitee.com>
2022-10-25 08:13:18 +0000

    更新v2版本，通过控件实现
    Signed-off-by: zhen365 <5002779+zhen365@user.noreply.gitee.com>

---
+++ hesuan_v2.js
@@ -0,0 +1,53 @@
+sleep(2000);
+unLock("000000");
+zhbOpenHesuan();
+getScreen();
+sendScreen("微信文件助手");
+
+
+// 唤醒设备+单指上滑+密码解锁
+function unLock(passwd) {
+    device.wakeUp();
+    sleep(1000);
+    gestures([200,[100, 2000], [100, 100]]);
+    sleep(1000);
+    for (var i=0; i<passwd.length; i++) {
+        click(passwd[i]);
+    }
+    toastLog("解锁成功");
+    sleep(1000);
+}
+
+// 进入郑好办核酸结果界面
+function zhbOpenHesuan() {
+    launchApp("郑好办");
+    text("郑好码").waitFor();
+    toast("进入郑好办");
+    click("郑好码")
+    text("核酸检测结果").waitFor();
+    click("核酸检测结果")
+    text("个人信息").waitFor();
+    toast("进入核酸界面");   
+}
+
+function getScreen() {
+    threads.start(function () {
+        var beginBtn = classNameContains("Button").textContains("立即开始").findOne(2000);
+        if (beginBtn) {
+            sleep(500)
+            beginBtn.click();
+        }
+    });
+    requestScreenCapture();
+    sleep(2000);
+    captureScreen("/sdcard/DCIM/hesuan.jpg");
+}
+
+function sendScreen(userName) {
+    launchApp("微信");
+    text(userName).waitFor();
+    click(userName);
+    className("android.widget.EditText").findOne(1000).setText("/sdcard/DCIM/hesuan.jpg");
+    click("确定");
+    toast("发送成功");
+}
